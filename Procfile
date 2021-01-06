a2b52dab1bc41e9f9e7314f2e1b5e48bf34064d4
Christoph Stade <christoph-stade@outlook.de>
2021-01-06 12:28:47 +0100

    Initial commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn pyWeb.wsgi --log-file -
--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -1,3 +1,5 @@
+import os
+
 """
 Django settings for personal_portfolio project.
 
@@ -25,11 +27,13 @@ SECRET_KEY = '8ap*-#r*)rq2j+p2#!8b5jq*s!%_p7e7=o2(h06m7^+7$i*ehs'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
-STATICFILES_DIRS = [
-    "C:/Users/Chris/PycharmProjects/pyWeb/personal_portfolio/static"
+ALLOWED_HOSTS = [
+    'christoph-stade-pyweb.herokuapp.com'
 ]
+
+#STATICFILES_DIRS = [
+#    "C:/Users/Chris/PycharmProjects/pyWeb/personal_portfolio/static"
+#]
 
 # Application definition
 
@@ -47,6 +51,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -120,8 +125,22 @@ USE_L10N = True
 
 USE_TZ = True
 
+## added for heroku
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.1/howto/static-files/
-
+# https://docs.djangoproject.com/en/1.11/howto/static-files/
+PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
+STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Extra lookup directories for collectstatic to find static files
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
+#  Add configuration for static files storage using whitenoise
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+import dj_database_url
+prod_db  =  dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
