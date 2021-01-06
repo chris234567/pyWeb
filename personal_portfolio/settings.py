ecc82d4203028ccd3a3592efed33985f139f8c6c
Christoph Stade <christoph-stade@outlook.de>
2021-01-06 20:46:32 +0100

    stuff

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -17,12 +17,10 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-
-TOKEN = os.getenv("SECRET_TOKEN")
+TOKEN = os.getenv("TOKEN")
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = TOKEN
 
