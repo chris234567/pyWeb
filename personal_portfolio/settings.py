d971aac0327be93f4b440e8e4639ff13a07f9410
Christoph Stade <christoph-stade@outlook.de>
2021-01-06 20:54:19 +0100

    stuff

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-TOKEN = os.getenv("TOKEN")
+TOKEN = os.getenv("SECRET_TOKEN")
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = TOKEN
 
