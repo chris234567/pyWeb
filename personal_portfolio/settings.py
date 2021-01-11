efa2bb6ae132487550c59c8d1a9e824a9b8ddc85
Christoph Stade <christoph-stade@outlook.de>
2021-01-11 09:20:38 +0100

    a bug

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -8,9 +8,9 @@ SECRET_KEY = TOKEN
 DEBUG = False
 
 ALLOWED_HOSTS = [
-    'christoph-stade-pyweb.herokuapp.com',
-    'www.christoph-stade.info',
-    '127.0.0.1', # for local development
+    'christoph-stade-pyweb.herokuapp.com/',
+    'www.christoph-stade.info/',
+    '127.0.0.1/', # for local development
 ]
 
 INSTALLED_APPS = [
